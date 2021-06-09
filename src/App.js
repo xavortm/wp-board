3d539c91bffcb0672ccf839b31055e1f2f0b2141
Alex Dimitrov <xavortm@gmail.com>
2021-06-09 08:32:45 +0300

    Cleanup and add initial dump of routing setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useContext, useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProvideAuth>
+      <ChakraProvider>
+        <Router>
+          <AuthButton />
+
+          <Switch>
+            <Route exact path="/">
+              <h1>Home page.</h1>
+            </Route>
+
+            <Route path="/login">
+              <h1>Login page</h1>
+            </Route>
+
+            <PrivateRoute path="/protected">
+              <h1>Protected page</h1>
+            </PrivateRoute>
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </ProvideAuth>
   );
-}
+};
+
+const authContext = createContext();
+
+const ProvideAuth = ({ children }) => {
+  const auth = useProvideAuth();
+
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
+
+const AuthButton = () => {
+  let history = useHistory();
+  let auth = useAuth();
+
+  return auth.user ? <p>Welcome</p> : <p>You are not logged in.</p>;
+};
+
+const useAuth = () => {
+  return useContext(authContext);
+};
+
+const useProvideAuth = () => {
+  const [user, setUser] = useState(null);
+
+  const signin = (cb) => {
+    return () => {
+      setUser("user");
+      cb();
+    };
+  };
+
+  const signout = (cb) => {
+    return () => {
+      setUser(null);
+      cb();
+    };
+  };
+
+  return {
+    user,
+    signin,
+    signout,
+  };
+};
+
+const PrivateRoute = ({ children, ...otherParams }) => {
+  let auth = useAuth();
+
+  return (
+    <Route
+      {...otherParams}
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default App;
